c780119b43597fac54b1f28ece4309e8716a0c60
Cezar Cocu <cezarc1@users.noreply.github.com>
2025-06-12 19:30:37 -0400

    Adds more doc

--- src/FaceDetector.ts
+++ src/FaceDetector.ts
@@ -1,5 +1,6 @@
 import {
   Detection,
+  Embedding,
   FaceDetector as FaceDetectorMediaPipe,
   FilesetResolver,
   ImageEmbedder,
@@ -16,6 +17,30 @@ const DEFAULT_DETECTION_MODEL =
   'https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite';
 const DEFAULT_WASM_PATH = 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm';
 
+/**
+ * A class for detecting and embedding faces.
+ * 
+ * @example
+ * ```ts
+ * const faceDetector = new FaceDetector({
+ *   device: 'GPU',
+ *   mode: 'IMAGE',
+ *   minDetectionConfidence: 0.5,
+ * });
+ * await faceDetector.initialize();
+ * const detections = await faceDetector.detectFromImage(imageElement);
+ * const embeddings = await faceDetector.embed({
+ *   source: imageElement,
+ *   detection: detections[0],
+ * });
+ * const similarity = FaceDetector.cosineSimilarity(embeddings[0], embeddings[1]);
+ * if (similarity > 0.5) {
+ *   console.log('The faces are similar');
+ * } else {
+ *   console.log('The faces are different');
+ * }
+ * ```
+ */
 export class FaceDetector {
   private faceDetector: FaceDetectorMediaPipe | null = null;
   private faceEmbedder: ImageEmbedder | null = null;
@@ -23,20 +48,16 @@ export class FaceDetector {
   private _state: FaceDetectorState = 'not_initialized';
   private _error: Error | null = null;
 
+  constructor(options: FaceDetectionOptions) {
+    this.options = options;
+  }
+
   get state() {
     return this._state;
   }
 
   get error() {
     return this._error;
-  }
-
-  /**
-   * Creates a new face detector.
-   * @param options - The options for the face detector.
-   */
-  constructor(options: FaceDetectionOptions) {
-    this.options = options;
   }
 
   async initialize() {
@@ -83,8 +104,18 @@ export class FaceDetector {
 
   /**
    * Detects faces from an image element.
+   * 
+   * @example
+   * ```ts
+   * const detections = await faceDetector.detectFromImage(imageElement);
+   * if (detections.length > 0) {
+   *   console.log('The image contains ' + detections.length + ' faces');
+   * } else {
+   *   console.log('The image does not contain any faces');
+   * }
+   * ```
    * @param imageElement - The image element to detect faces from.
-   * @returns The detections.
+   * @returns The detections. If no faces are detected, returns an empty array.
    */
   async detectFromImage(imageElement: HTMLImageElement): Promise<FaceDetection[]> {
     if (this.state !== 'initialized') {
@@ -95,6 +126,16 @@ export class FaceDetector {
 
   /**
    * Detects faces from a video element.
+   * 
+   * @example
+   * ```ts
+   * const detections = await faceDetector.detectFromVideo(videoElement, timestamp);
+   * if (detections.length > 0) {
+   *   console.log('The video contains ' + detections.length + ' faces');
+   * } else {
+   *   console.log('The video does not contain any faces');
+   * }
+   * ```
    * @param videoElement - The video element to detect faces from.
    * @param timestamp - The timestamp of the video element.
    * @returns The detections.
@@ -147,9 +188,32 @@ export class FaceDetector {
   }
 
   /**
-   * Embeds a face into a vector.
+   * Embeds a detected face into a tensor.
+   * The resulting tensor can be then compared to other embeddings using cosine similarity or other distance metrics.
+   * 
+   * @example
+   * ```ts
+   * const embedding = await faceDetector.embed({
+   *   source: imageElement,
+   *   detection: faceDetectedFromImageElement,
+   * });
+   * 
+   * const embedding2 = await faceDetector.embed({
+   *   source: videoElement,
+   *   detection: faceDetectedFromVideoElement,
+   *   timestamp: performance.now()
+   * });
+   *
+   * const similarity = FaceDetector.cosineSimilarity(embedding, embedding2);
+   * if (similarity > 0.5) {
+   *   console.log('The faces are similar');
+   * } else {
+   *   console.log('The faces are different');
+   * }
+   * ```
    * @param request - The request for embedding a face.
-   * @returns The embedded face vector.
+   * @returns The embedded face tensor or null if no bounding box is found.
+   * 
    * @throws Error if face embedder is not initialized (embeddingModelPath not provided)
    */
   async embed(request: EmbeddingRequest): Promise<EmbeddingResult | null> {
@@ -195,5 +259,26 @@ export class FaceDetector {
     }
 
     throw new Error('Invalid source type');
+  }
+
+  /**
+   * Computes the cosine similarity between two embeddings.
+   * The cosine similarity score is between -1 and 1, where 1 means the embeddings are identical, and -1 means they are completely different.
+   * 
+   * @example
+   * ```ts
+   * const similarity = FaceDetector.cosineSimilarity(faceEmbedding1, faceEmbedding2);
+   * if (similarity > 0.5) {
+   *   console.log('The embeddings are similar');
+   * } else {
+   *   console.log('The embeddings are different');
+   * }
+   * ```
+   * @param a - The first embedding.
+   * @param b - The second embedding.
+   * @returns The cosine similarity score.
+   */
+  static cosineSimilarity(a: Embedding, b: Embedding): number {
+    return ImageEmbedder.cosineSimilarity(a, b);
   }
 }
